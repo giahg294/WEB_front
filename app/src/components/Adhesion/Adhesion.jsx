6f531da8e63a81617eff76b2a2d6e3fe4c266995
bmuccini <muccini.bianca@gmail.com>
2025-03-31 16:25:13 +0200

    Ajout Infobar dans les pages

--- app/src/components/Adhesion/Adhesion.jsx
+++ app/src/components/Adhesion/Adhesion.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Navbar from '../Navbar';
 import './Adhesion.css';
+import Infobar from '../Infobar';
 
 function Adhesion() {
   return (
-    <div className="Home">
+    <div className="Adhesion">
       <Navbar />
+      
       {/* <img src="/eau2.jp:g" alt="Fond Eau" className="background-image" /> */}
+      <Infobar />
     </div>
   );
 }
