8edb2b5e138a4d2926e5e3782701e8c22e0395dc
bmuccini <muccini.bianca@gmail.com>
2025-03-31 22:23:11 +0200

    Ajout liens contacts

--- app/src/components/Infobar.jsx
+++ app/src/components/Infobar.jsx
@@ -6,10 +6,22 @@ function Infobar() {
     return(
         <footer className="infobar">
             <div className="infobar-container">
-                <p>© 2025 INSAQUATIC</p>
-                <div className="infobar-links">
-                    <Link to="/contacts">Contacts</Link>
-                    <Link to="/mentions">Mentions légales</Link>
+
+                <div className="infobar-social">
+                <span className="infobar-contact">Contacts</span>
+                    <a
+                        href="https://www.instagram.com/insaquatic/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="instagram-link"
+                    >
+                        <i className="fab fa-instagram"></i> @insaquatic
+                    </a>
+                    <span className="infobar-mail">insaquatic@gmail.com</span>
+                </div>
+
+                <div className="infobar-right">
+                    <p>© 2025 INSAQUATIC</p>
                 </div>
             </div>
         </footer>
