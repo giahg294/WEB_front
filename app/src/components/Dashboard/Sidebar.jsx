8069b75ddc5082ae2fb0c3fb04114bc22fb90017
giahg294 <giahoang294@gmail.com>
2025-04-04 15:21:20 +0200

    sidebar icon

--- app/src/components/Dashboard/Sidebar.jsx
+++ app/src/components/Dashboard/Sidebar.jsx
@@ -17,21 +17,21 @@ function Sidebar() {
               to="/admin/annual"
               className={`items ${location.pathname === "/admin/annual" ? "active" : ""}`}
             >
-              <img src="/maison.png" alt="home" className="icon" />
+              <img src="/home.svg" alt="home" className="icon" />
               Statistiques annuelles
             </Link>
             <Link
               to="/admin/seance"
               className={`items ${location.pathname === "/admin/seance" ? "active" : ""}`}
             >
-              <img src="/stat.png" alt="stat" className="icon" />
+              <img src="/stats.svg" alt="stat" className="icon" />
               Séances
             </Link>
             <Link
               to="/admin/paiements"
               className={`items ${location.pathname === "/admin/paiements" ? "active" : ""}`}
             >
-              <img src="/paiement.png" alt="money" className="icon" />
+              <img src="/money.svg" alt="money" className="icon" />
               Paiements
             </Link>
           </li>
